a52ef92a8a2c595bc42215d459e2e608c24d778f
haohan <han.chang@i2csolution.com>
2024-02-27 15:29:53 +0800

    expose ref and add onClick in colorRadio

--- components/input/colorRadio.js
+++ components/input/colorRadio.js
@@ -1,23 +1,27 @@
+import { forwardRef } from "react";
 import Image from "next/image";
 import { FormGroup, FormCheck, FormLabel } from "react-bootstrap";
 
-const ColorCheck = ({ id, label, src, name }) => {
+const ColorCheck = forwardRef(function ColorCheck(
+  { id, label, src, name, onClick },
+  ref
+) {
   return (
-    <>
-      <FormGroup className="text-center" controlId={id}>
-        <FormCheck className="d-inline-block position-relative lh-1">
-          <FormCheck.Input
-            type="radio"
-            name={name}
-            value={id}
-            className={`m-0 rounded-1`}
-          />
-          <Image alt="color check box image" fill sizes="1em" src={src} />
-        </FormCheck>
-        <FormLabel className="d-block m-0 fs-6-xs">{label}</FormLabel>
-      </FormGroup>
-    </>
+    <FormGroup className="text-center" controlId={id}>
+      <FormCheck className="d-inline-block position-relative lh-1">
+        <FormCheck.Input
+          ref={ref}
+          type="radio"
+          name={name}
+          value={id}
+          onClick={onClick}
+          className={`m-0 rounded-1`}
+        />
+        <Image alt="color check box image" fill sizes="1em" src={src} />
+      </FormCheck>
+      <FormLabel className="d-block m-0 fs-6-xs">{label}</FormLabel>
+    </FormGroup>
   );
-};
+});
 
 export default ColorCheck;
