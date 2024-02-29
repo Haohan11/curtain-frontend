2463971bb07f2f06038f43b9979b6b25ddd2a0e9
haohan <han.chang@i2csolution.com>
2024-02-29 18:32:08 +0800

    add pagination component without function

--- components/pagination.js
+++ components/pagination.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { FormControl } from "react-bootstrap";
+
+import ArrowLeft from "@/icon/arrow-left";
+import ArrowRight from "@/icon/arrow-right";
 
 const pagination = () => {
   return (
-    <div>pagination</div>
-  )
-}
+    <div className="w-100 px-4 position-absolute bottom-0 left-0 fs-6-sm flex-center" style={{height: '100px'}}>
+      <ArrowLeft width={36} className="me-2" />
+      <span className="mx-3">1</span>
+      <span className="mx-3">2</span>
+      <span className="mx-3">3</span>
+      <span className="mx-3">...</span>
+      <ArrowRight width={36} className="ms-2" />
+      <div className="position-absolute end-0 pe-6 text-textblue">
+        <span>選擇頁數</span>
+        <FormControl
+          className="d-inline-block mx-2 text-center text-textgrey rounded-3"
+          style={{ height: "45px", width: "48px" }}
+        />
+        <span>/ 200</span>
+      </div>
+    </div>
+  );
+};
 
-export default pagination
+export default pagination;
--- components/productList.js
+++ components/productList.js
@@ -1,35 +1,47 @@
 import Image from "next/image";
 import { Col, FormSelect, Row } from "react-bootstrap";
 
+import Pagination from "@/components/pagination";
+
 import Cross from "@/icon/cross";
 
 const ProductList = () => {
   return (
-    <Row className="row-cols-5 p-3 px-2 g-0 justify-content-around">
-      {[...new Array(5)].map((_, index) => (
-        <Col key={index} className="px-2" style={{maxWidth: "185px"}}>
-          <div
-            className="position-relative w-100"
-            style={{ aspectRatio: "185 / 120" }}
-          >
-            <Image alt="product image" src={"/image/xmas.jpg"} fill />
-          </div>
-          <p className="p-2 m-0 text-center text-darkblue fw-bold">
-            Product name
-          </p>
-          <FormSelect className="text-textgrey">
-            <option>選擇顏色</option>
-          </FormSelect>
-          <div className="flex-center fw-bold text-checkboxblue py-2">
-            <div className="cursor-pointer">
-              <Cross />
-              <span className="ms-2">加入組合</span>
+    <div className="position-relative h-100">
+      <Row
+        className="row-cols-5 pt-3 px-2 g-0 justify-content-between mx-auto"
+        style={{ maxWidth: "1080px" }}
+      >
+        {[...new Array(5)].map((_, index) => (
+          <Col key={index} className="" style={{ maxWidth: "185px" }}>
+            <div
+              className="position-relative w-100"
+              style={{ aspectRatio: "185 / 120" }}
+            >
+              <Image
+                alt="product image"
+                sizes="185px"
+                src={"/image/xmas.jpg"}
+                fill
+              />
             </div>
-          </div>
-        </Col>
-      ))}
-
-    </Row>
+            <p className="p-2 m-0 text-center text-darkblue fw-bold">
+              Product name
+            </p>
+            <FormSelect className="text-textgrey">
+              <option>選擇顏色</option>
+            </FormSelect>
+            <div className="flex-center fw-bold text-checkboxblue py-2">
+              <div className="cursor-pointer">
+                <Cross />
+                <span className="fs-6-sm ms-2">加入組合</span>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+      <Pagination />
+    </div>
   );
 };
 
